4e4bcbe238a1c27be8d050808ac12381f94827a5
Niemelä Petri <petri.niemela@bilot.fi>
2017-07-27 17:09:24 +0300

    database connection

---
+++ src/main/java/fi/bilot/database/DbContract.java
@@ -0,0 +1,9 @@
+package fi.bilot.database;
+
+public interface DbContract
+{
+	public static final String HOST = "jdbc:postgresql://localhost:5432/";
+	public static final String DB_NAME = "testdata";
+	public static final String USERNAME = "postgres";
+	public static final String PASSWORD = "password";
+}
